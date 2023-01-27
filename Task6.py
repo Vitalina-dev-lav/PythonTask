8586b138772d5d3c7de792e7761c2ec4b5ceca12
Vitalina <vitalinabatrakova@gmail.com>
2023-01-27 19:40:37 +0300

    Task6

--- Task6.py
+++ Task6.py
@@ -7,3 +7,10 @@
 
 # 385916 -> yes
 # 123456 -> no
+
+print ('Введите номер билета: ')
+n=input()
+if (int(n[0])+int(n[1])+int(n[2])==int(n[3])+int(n[4])+int(n[5])):
+ print("Yes")
+else:
+ print("No")
