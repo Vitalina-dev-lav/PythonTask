ce446e80b88daeee6ef3423bc6efb5a9aaa629e5
Vitalina <vitalinabatrakova@gmail.com>
2023-02-13 19:05:16 +0300

    ClassWork

---
+++ Task25.py
@@ -0,0 +1,19 @@
+# Задача №25. Решение в группах
+# Напишите программу, которая принимает на вход
+# строку, и отслеживает, сколько раз каждый символ
+# уже встречался. Количество повторов добавляется к
+# символам с помощью постфикса формата _n.
+# Input: a a a b c a a d c d d
+# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+# Для решения данной задачи используйте функцию
+# .split()
+
+print("Введите символы: ")
+text = input().split()
+result = dict()
+for chair in text:
+    if chair in result:
+        print(f"{chair}_{result[chair]}", end=" ")
+    else:
+        print(f"{chair}", end= " ")
+    result[chair] = result.get(chair, 0)+1
