065fda33cdbd2ee1df439648b24d7e57eebc1354
Vitalina <vitalinabatrakova@gmail.com>
2023-02-15 20:17:56 +0300

    Homework

--- Task26.py
+++ Task26.py
@@ -3,3 +3,14 @@
 # помощью рекурсии.
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+def power(a, b):
+
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    else:
+        return power(a, b-1)*a
+    
+print(power(a=int(input("Enter number A: ")), b = int(input("Enter number B3: "))))
--- Task28.py
+++ Task28.py
@@ -4,3 +4,15 @@
 # Также нельзя использовать циклы.
 # 2 2
 # 4
+
+a = int(input('Enter number a: '))
+b = int(input('Enter number b: '))
+ 
+ 
+def sum(a, b):
+    if b == 0:
+        return a
+    return sum(a+1, b-1)
+ 
+ 
+print(sum(a, b))
